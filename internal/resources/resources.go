597150b2104c43b7796a101a33ea2bda44f17034
Faiz <64007466+faizahmd2@users.noreply.github.com>
2026-09-20 01:15:43 +0530

    cleanup: remove obsolete internal/resources/resources.go

--- internal/resources/resources.go
+++
@@ -1,8 +0,0 @@
-package resources
-
-import "embed"
-
-// Files contains install-time templates.
-//
-//go:embed systemd/diagnos-check.service.tmpl systemd/diagnos-check.timer.tmpl sudoers.tmpl
-var Files embed.FS
