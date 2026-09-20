6b3144dee8e67be6b8ff67f389fdbe9ff26445e7
Faiz <64007466+faizahmd2@users.noreply.github.com>
2026-09-20 21:14:22 +0530

    rename: update pinproc module and identifiers

--- internal/capability/spec/spec.go
+++ internal/capability/spec/spec.go
@@ -1,8 +1,8 @@
 package spec
 
 import (
-	"github.com/faizahmd2/vm-native-diagnos/internal/contract"
-	"github.com/faizahmd2/vm-native-diagnos/internal/source"
+	"github.com/faizahmd2/pinproc/internal/contract"
+	"github.com/faizahmd2/pinproc/internal/source"
 	"time"
 )
 
