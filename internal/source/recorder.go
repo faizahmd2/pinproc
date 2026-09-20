de2787c387da83d90ce1227f597dbc052b887e45
Faiz <64007466+faizahmd2@users.noreply.github.com>
2026-09-20 21:16:10 +0530

    rename: update pinproc module and identifiers

--- internal/source/recorder.go
+++ internal/source/recorder.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"github.com/faizahmd2/vm-native-diagnos/internal/contract"
+	"github.com/faizahmd2/pinproc/internal/contract"
 	"os"
 	"path/filepath"
 	"sort"
